02bdad724799604d4ebde13f5d7d6c96555ecb47
Jesus Alcocer Tagua <jesalctag@gmail.com>
2021-10-03 15:56:06 +0200

    web changes

--- GH-page/knitt_me.R
+++ GH-page/knitt_me.R
@@ -1,12 +1,16 @@
-#knitt the file on its current folder to test its view
-rmarkdown::render('Index.Rmd',
-                  output_file = paste('index', 
-                                      '.html', sep=''))
+#RPi
 
 #Get the current wd as the directory of this file
 wd<-dirname(rstudioapi::getActiveDocumentContext()$path)
-#Set the destination to the previous folder
-Financial_analysis_R = substring(wd,1, nchar(wd)-8)
+setwd(wd)
+#knitt the file on its current folder to test its view
+rmarkdown::render('RPi_index.Rmd',
+                  output_file = paste('index', 
+                                      '.html', sep=''))
+
+
+#Get the destination to the previous folder
+RPi = substring(wd,1, nchar(wd)-8)
 
 #Copy the newest html file
-file.copy(from = gsub(" ", "", paste(wd,"/index.html")),Financial_analysis_R, overwrite = T)
+file.copy(from = gsub(" ", "", paste(wd,"/index.html")),RPi, overwrite = T)
